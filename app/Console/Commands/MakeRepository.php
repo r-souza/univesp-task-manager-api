17a8a2fd52d2ea38572cf3cbe7df2352183158f6
Rodrigo de Souza <contato@rodrigosouza.eti.br>
2023-09-17 23:41:45 +0000

    Add make:repository command

---
+++ app/Console/Commands/MakeRepository.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+use Symfony\Component\Console\Input\InputOption;
+use Illuminate\Console\Command;
+
+class MakeRepository extends GeneratorCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $name = 'make:repository';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create an Repository Class for given Model extending the BaseRepository';
+
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Repository';
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return __DIR__.'/stubs/repository.stub';
+    }
+
+    /**
+     * Build the class with the given name.
+     *
+     * @param  string  $name
+     * @return string
+     */
+    protected function buildClass($name)
+    {
+        $model = $this->option('model')
+            ? $this->qualifyClass($this->option('model'))
+            : $name;
+
+        #Get Model name without namespace
+        $class = str_replace($this->getNamespace($model).'\\', '', $model);
+
+        return str_replace(
+            ['DummyModel', 'DModelName' ,'dummyVar'],
+            [$model, $class ,strtolower($class)],
+            parent::buildClass($name)
+        );
+    }
+
+    /**
+     * Get the destination class path.
+     *
+     * @param  string  $name
+     * @return string
+     */
+    protected function getPath($name)
+    {
+        $name = str_replace(
+            ['\\', '/'],
+            '',
+            $this->argument('name')
+        );
+
+        return $this->laravel->basePath()."/app/Repositories/{$name}Repository.php";
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['model', 'm', InputOption::VALUE_OPTIONAL, 'The name of the model'],
+        ];
+    }
+}
