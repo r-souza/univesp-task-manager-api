5230ecb6400a5517f484bd815d300692af0394c9
= <contato@rodrigosouza.eti.br>
2023-09-28 21:00:43 +0000

    Add Tasks management #4

---
+++ app/Models/Task.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
+
+class Task extends Model implements Auditable
+{
+    use \OwenIt\Auditing\Auditable;
+    use SoftDeletes;
+    use HasFactory;
+
+    /**
+    * The attributes that are mass assignable.
+    *
+    *@var array
+    */
+    protected $fillable = [
+        'name',
+        'description',
+        'completed',
+        'priority_id',
+        'project_id',
+        'status_id',
+        'user_id'
+    ];
+
+    public function priority()
+    {
+        return $this->belongsTo('App\Models\Priority');
+    }
+
+    public function project()
+    {
+        return $this->belongsTo('App\Models\Project');
+    }
+
+    public function status()
+    {
+        return $this->belongsTo('App\Models\Status');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PriorityController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\StatusController;
-use App\Models\Status;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,6 +28,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
         'priorities' => \App\Http\Controllers\PriorityController::class,
         'projects' => \App\Http\Controllers\ProjectController::class,
         'status' => \App\Http\Controllers\StatusController::class,
+        'tasks' => \App\Http\Controllers\TaskController::class,
     ]);
 
     /**
@@ -36,6 +37,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/priorities/{id}/restore', [PriorityController::class, 'restore'])->name('priorities.restore');
     Route::post('/projects/{id}/restore', [ProjectController::class, 'restore'])->name('projects.restore');
     Route::post('/status/{id}/restore', [StatusController::class, 'restore'])->name('status.restore');
+    Route::post('/tasks/{id}/restore', [TaskController::class, 'restore'])->name('tasks.restore');
 
     /**
      * Trashed routes
@@ -43,4 +45,5 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/priorities/{id}/trashed', [PriorityController::class, 'showTrashed'])->name('priorities.showTrashed');
     Route::get('/projects/{id}/trashed', [ProjectController::class, 'showTrashed'])->name('projects.showTrashed');
     Route::get('/status/{id}/trashed', [StatusController::class, 'showTrashed'])->name('status.showTrashed');
+    Route::get('/tasks/{id}/trashed', [TaskController::class, 'showTrashed'])->name('tasks.showTrashed');
 });
