7301405a51f5d8f15a3b5622e33536eb8b496cac
Rodrigo de Souza <contato@rodrigosouza.eti.br>
2023-10-30 05:36:37 +0000

    Add filters to Task pagination #4

--- app/Repositories/TaskRepository.php
+++ app/Repositories/TaskRepository.php
@@ -12,9 +12,45 @@ class TaskRepository extends BaseRepository
      * TaskRepository constructor
      * @param Task $task
      */
-     public function __construct(Task $task)
-     {
-         $this->model = $task;
-     }
+    public function __construct(Task $task)
+    {
+        $this->model = $task;
+    }
+
+    public function paginate(
+        $items = null,
+        $sortColumn = 'id',
+        $sortOrder = 'asc',
+        $filterField = 'name',
+        $filter = null,
+        $statusId = null,
+        $projectId = null,
+        $priorityId = null,
+        $userId = null
+    ) {
+        $model = $this->model->orderBy($sortColumn, $sortOrder);
+
+        if ($filter) {
+            $model->where($filterField, 'like', '%' . $filter . '%');
+        }
+
+        if ($statusId) {
+            $model->where('status_id', $statusId);
+        }
+
+        if ($projectId) {
+            $model->where('project_id', $projectId);
+        }
+
+        if ($priorityId) {
+            $model->where('priority_id', $priorityId);
+        }
+
+        if ($userId) {
+            $model->where('user_id', $userId);
+        }
+
+        return $model->with('status:id,name')->with('priority:id,name')->with('user:id,name')->paginate($items);
+    }
 
 }
